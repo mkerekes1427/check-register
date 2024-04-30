46aac723d7f94e4a6c42cf61d9c75b35428e0536
mkerekes1427 <mkerekes53@gmail.com>
2024-04-30 02:41:42 -0400

    early styling mui

--- registry/src/app/components/registry.tsx
+++ registry/src/app/components/registry.tsx
@@ -1,10 +1,70 @@
-import { FormControl, useFormControlContext } from '@mui/base/FormControl';
-import { Input, inputClasses } from '@mui/base/Input';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 export default function Registry() {
-    return(
-        <FormControl>
-            <Input></Input>
-        </FormControl>
-    )
+ 
+  return (
+      <Container component="main" maxWidth="xl">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+        <Link href="https://espn.com" sx={{textDecoration: "none"}}>
+            <Typography variant="h2" sx={{color : "#00a88c"}}>
+                Check Registry
+            </Typography>
+          </Link>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+  );
 }
