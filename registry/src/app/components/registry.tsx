58ab5b8ce4c2592d3e358db459c41171d4c52fcd
mkerekes1427 <mkerekes53@gmail.com>
2024-04-30 18:18:26 -0400

    ui working

--- registry/src/app/components/registry.tsx
+++ registry/src/app/components/registry.tsx
@@ -1,19 +1,92 @@
+'use client';
+
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormLabel from '@mui/material/FormLabel';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Alert from '@mui/material/Alert';
+import addTransaction from '../utils/addTransaction';
+
+import { useState } from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { Dayjs } from 'dayjs';
+
+
+type registryForm = {
+  checkNo: string,
+  transaction: string,
+  amount: string,
+  date: Dayjs | null,
+  description: string,
+  category: string,
+  submitted: boolean,
+  error: boolean
+}
 
 export default function Registry() {
+
+  const [registry, setRegistry] = useState<registryForm>({
+                          checkNo: "",
+                          transaction: "Debit",
+                          amount: "0",
+                          date: null,
+                          description: "",
+                          category: "",
+                          submitted: false,
+                          error: false
+                        });
+
+
+  let handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRegistry({...registry, 
+      [e.target.name] : e.target.value});
+  }
+
+  let handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log("You submitted");
+
+    registry.date === null || registry.description === "" || registry.amount === ""
+      ? setRegistry({...registry, error:true, submitted:true}) : setRegistry({...registry, error:false, submitted:true});
+
+    setTimeout(() => 
+      setRegistry({...registry, error:false, submitted:false}), 
+    3500)
+
+    addTransaction();
+
+  }
+
+  let displayAlert = () => {
+
+    if (!registry.submitted)
+      return;
+
+    else if (registry.error && registry.submitted)
+      return <Alert variant="outlined" severity="error" sx={{textAlign: "center", fontSize: "28px"}}>Fill out all the required fields.</Alert>;
+
+    else
+      return <Alert variant="outlined" severity="success" sx={{textAlign: "center", fontSize: "28px"}}>Transaction Uploaded Successfully.</Alert>
+
+  }
+
+
+  console.log(registry);
  
   return (
       <Container component="main" maxWidth="xl">
         <CssBaseline />
+        {displayAlert()}
         <Box
           sx={{
             marginTop: 10,
@@ -22,7 +95,7 @@ export default function Registry() {
             alignItems: 'center'
           }}
         >
-        <Link href="https://espn.com" sx={{textDecoration: "none"}}>
+        <Link href="https://espn.com" sx={{textDecoration: "none", textAlign: "center"}}>
             <Typography variant='h2' sx={{color : "#00a88c", fontSize: "80px", mb: 8}}>
                 Check Registry
             </Typography>
@@ -35,32 +108,61 @@ export default function Registry() {
               label="Check Number (optional)"
               name="checkNo"
               autoFocus
+              sx={{mb: 5}}
+              onChange={handleChange}
             />
+            <FormLabel id="demo-radio-buttons-group-label">Transaction Type</FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="Debit"
+                name="transaction"
+                value={registry.transaction}
+                onChange={handleChange}
+                sx = {{'& .MuiSvgIcon-root': {
+                  fontSize: 36,
+                  color: "#00796b"
+                },}}
+              >
+                <FormControlLabel value="Debit" control={<Radio />} label="Debit" />
+                <FormControlLabel value="Credit" control={<Radio />} label="Credit" />
+              </RadioGroup>
+            <Typography mt={3} sx={{color: "#00695c", fontSize: "18px"}}>Amount*</Typography>
+            <TextField required fullWidth inputProps=
+            {{ type: 'number',
+              min: 0
+
+            }} value={registry.amount} onChange={handleChange} 
+            name="amount" color="success"
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']} sx={{mt: 2, mb: 2}}>
+                <DatePicker label="Date" name="date" onChange={(val) => setRegistry({...registry, date: val})}/>
+              </DemoContainer>
+            </LocalizationProvider>
             <TextField
               margin="normal"
               required
               fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
+              name="description"
+              label="Description"
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              name="category"
+              label="Category (optional)"
+              onChange={handleChange}
             />
             <Button
               type="submit"
+              onClick={handleSubmit}
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2, fontSize: "24px" }}
             >
               Add Transaction
             </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
       </Container>
---
+++ registry/src/app/utils/addTransaction.tsx
@@ -0,0 +1,3 @@
+export default function addTransaction() {
+    console.log("Working ok")
+}
