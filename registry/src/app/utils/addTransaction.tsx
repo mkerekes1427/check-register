0bb8e027b43651ae76d394f59504e0c61eb6cf38
mkerekes1427 <mkerekes53@gmail.com>
2024-05-01 12:53:27 -0400

    testing keys

--- registry/src/app/utils/addTransaction.tsx
+++ registry/src/app/utils/addTransaction.tsx
@@ -17,6 +17,9 @@ export default async function addTransaction(formValues: registryForm) {
         range: "Sheet1"
       });
 
+      console.log(`Sheet id is ${process.env.SHEET_ID}`);
+      console.log(`credentials are ${process.env.GOOGLE_APPLICATION_CREDENTIALS}`);
+
       const rowNum = (getRes.data.values?.length as number) + 1;
 
       const checkNo = formValues.checkNo;
